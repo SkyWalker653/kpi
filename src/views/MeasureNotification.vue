d71cefa239e5fed144c0a73c7ae50ee9320ba4fa
Priya Ranjan <priyaranjan.653@gmail.com>
2019-04-10 23:08:52 +0530

    Update MeasureNotification.vue
    changes by sibu

--- src/views/MeasureNotification.vue
+++ src/views/MeasureNotification.vue
@@ -9,23 +9,60 @@
       :columns="columns"
       row-key="name"
       class="q-mt-md"
-    />
+    >
+     <q-td slot="body-cell-payload" slot-scope="props" :props="props">
+        <q-btn flat color="primary" label="View" @click="populateModalData(props.row.payload)"></q-btn>
+      </q-td>
+      <q-td slot="body-cell-message" slot-scope="props" :props="props">
+        <q-btn flat color="primary" label="View" @click="populateModalData(props.row.message)"></q-btn>
+      </q-td>
+    </q-table>
+    <q-modal v-model="showModal" :content-css="{minWidth: '50vw'}">
+      <q-modal-layout>
+        <q-toolbar slot="header">
+          <q-toolbar-title>Details</q-toolbar-title>
+        </q-toolbar>
+        <div class="q-pa-md">
+          <q-card>
+            <q-card-main>
+              <vue-json-pretty
+                :data="modalData">
+              </vue-json-pretty>
+            </q-card-main>
+          </q-card>
+        </div>
+        <q-toolbar slot="footer">
+          <q-toolbar-title>
+            <q-btn flat v-close-overlay label="close"></q-btn>
+          </q-toolbar-title>
+        </q-toolbar>
+      </q-modal-layout>
+    </q-modal>
   </q-page>
 </template>
 
 <script>
 import store from 'vuex-store'
+import VueJsonPretty from 'vue-json-pretty'
 import { required } from 'vuelidate/lib/validators'
 export default {
   data () {
     return {
+     showModal: false,
+     modalData:[],
       referenceId: '',
       measureNotifications: [],
       columns: [
-        { name: 'creationDate', label: 'Creation Date', field: 'creationDate', align: 'left' },
-        { name: 'message', label: 'Message', field: 'message', align: 'left' }
+        { name: 'ruleId', label: 'Rule Id', field: 'rule_id', align: 'left' },
+        { name: 'transactionId', label: 'Transaction Id', field: 'transactionId', align: 'left' },
+        { name: 'payload', label: 'Payload', field: 'payload', align: 'left' },
+        { name: 'message', label: 'Response', field: 'message', align: 'left' },
+        { name: 'creationDate', label: 'Creation Date', field: 'creationDate', align: 'left' }
       ]
     }
+  },
+  components: {
+    VueJsonPretty
   },
   validations: {
     referenceId: { required }
@@ -71,7 +108,13 @@ export default {
         .finally(() => {
           this.referenceId = ''
         })
+    },
+    populateModalData(payload){
+      //console.log(payload)
+      this.modalData = JSON.parse(payload)
+      this.showModal =true
     }
+
   }
 }
 </script>
